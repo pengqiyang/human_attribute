7c1b54e1c9904b204dd8732a426f14daa05b7f7b
qiyang <3222435823@qq.com>
2021-04-11 22:20:51 +0800

    version0

---
+++ test.sh
@@ -0,0 +1,1 @@
+python3 test.py PA100k --device=0,1,2,3 --model_name='resnet50' --train_split='train' --valid_split='test'
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-python train.py
+python3 train.py PA100k --device=0,1,2,3 --model_name='resnet50' --train_split='train' --valid_split='val'
