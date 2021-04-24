fd6afd58481515225a2cd1cf87f4048ace2c219f
pengqiyang <3222435823@qq.com>
2021-04-24 20:13:58 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -24,6 +24,6 @@
 
 #python3 train.py PA100k --batchsize=64 --loss='BCE_LOSS' --device='5' --save_path='resnet18_vit_v4' --model_name='resnet18_vit_v4' --train_split='train' --valid_split='val'
 
-python3 train.py PA100k --batchsize=64 --loss='KL_LOSS' --device='0,2' --save_path='resnet18_vit_v5' --model_name='resnet18_vit_v5' --train_split='train' --valid_split='val'
+python3 train.py PA100k --batchsize=64 --loss='KL_LOSS' --device='4,5' --save_path='resnet18_vit_v5' --model_name='resnet18_vit_v5' --train_split='train' --valid_split='val'
 
 #python3 train.py PA100k --batchsize=64 --loss='BCE_LOSS' --device='4' --save_path='resnet34' --model_name='resnet34' --train_split='train' --valid_split='val'
